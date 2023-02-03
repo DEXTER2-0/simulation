b7785cac2daad385559340f111b27821e5c3c909
Warintara Munsup <Cartooneiei555@hotmail.com>
2023-02-03 11:08:02 +0100

    modif erreur dans Capteur_de_distance.py

--- src/Capteur_de_distance.py
+++ src/Capteur_de_distance.py
@@ -1,9 +1,9 @@
 from math import pi,sqrt
 class Capteur_de_distance :
-   def __init__(self, distanceCaptable,distanceObstacle) :
+    def __init__(self, distanceCaptable,distanceObstacle) :
        self.distanceCaptable=distanceCaptable
        self.distanceObstacle=distanceObstacle
-
+    
     def distance(self,robot,obstacle):
         """
         Permet de calculer la distance entre le robot de l'obstable passés en parametre
