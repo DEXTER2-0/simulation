d1495ee022a8a2e1383596815013e9200fe81db3
Warintara Munsup <Cartooneiei555@hotmail.com>
2023-02-03 11:02:27 +0100

    ajout methode distance

--- src/Capteur_de_distance.py
+++ src/Capteur_de_distance.py
@@ -1,6 +1,17 @@
-from math import pi
+from math import pi,sqrt
 class Capteur_de_distance :
    def __init__(self, distanceCaptable,distanceObstacle) :
        self.distanceCaptable=distanceCaptable
        self.distanceObstacle=distanceObstacle
+
+    def distance(self,robot,obstacle):
+        """
+        Permet de calculer la distance entre le robot de l'obstable passés en parametre
+        """
+        xr = robot.pos_x
+        yr = robot.pos_y
+        xo = obstacle.x
+        yo = obstacle.y
+        return sqrt((xo-xr)**2+(yo-yr)**2)
+        
       
