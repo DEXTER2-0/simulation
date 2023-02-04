b345ce2acf5f1c8f5ac5e55a0d998e784477a84c
Yassin Lahbib <yaaston94@gmail.com>
2023-02-04 22:18:58 +0100

    modig Graphique

--- src/TestGraphique.py
+++ src/TestGraphique.py
@@ -1,12 +1,13 @@
 from Robot import * # Permet d'utiliser la classe Robot se trouvant dans le meme repertoire
 from Roue import * # Permet d'utiliser la classe Roue se trouvant dans le meme repertoire
 import time #pour pouvoir controler le temps de la boucle while True
+from math import *
 
+##----- Importation des Modules -----##
+from tkinter import * 
 
-# Initialisation des constantes du robot
-RAYON_DES_ROUES_CM = 3
-VITESSE_MAX_TOUR_PAR_SEC = 5
-RAYON_ROBOT_CM = 8
+def ang(T_en_sec): # calcul vitesse angulaire
+    return (2*pi/T_en_sec)*RAYON_DES_ROUES_CM*0.01
 
 # instanciation d'un robot, prenant en parametre les deux roue créer précédemment
 robot = Robot(RAYON_DES_ROUES_CM, VITESSE_MAX_TOUR_PAR_SEC, RAYON_ROBOT_CM)
