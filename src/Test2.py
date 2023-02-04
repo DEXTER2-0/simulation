0448c286f55929fcf22888c051cbb413fbd91658
Yassin Lahbib <yaaston94@gmail.com>
2023-02-04 17:29:26 +0100

    modif Test2 et Robot

--- src/Test2.py
+++ src/Test2.py
@@ -1,6 +1,6 @@
 from Robot import * # Permet d'utiliser la classe Robot se trouvant dans le meme repertoire
 from Roue import * # Permet d'utiliser la classe Roue se trouvant dans le meme repertoire
-
+import time #pour pouvoir controler le temps de la boucle while True
 
 
 # Initialisation des constantes du robot
@@ -13,5 +13,8 @@ robot = Robot(RAYON_DES_ROUES_CM, VITESSE_MAX_TOUR_PAR_SEC, RAYON_ROBOT_CM)
 
 print(robot)
 
-
-
+while True :
+    robot.tourner2(2,2)
+    robot.nouvelle_position2(1)
+    time.sleep(1)
+    print(robot)
