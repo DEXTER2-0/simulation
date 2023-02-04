5abc9c1e707830ff2adccf3baa8c6c236fb74487
Warintara Munsup <Cartooneiei555@hotmail.com>
2023-02-04 22:13:15 +0100

    update simulation.py

--- src/Simulation.py
+++ src/Simulation.py
@@ -1,19 +1,16 @@
-from Roue import *
-from Obscle import *
+from Robot import *
+from Obstacle import *
 from Roue import *
 from Capteur_de_distance import *
 
 class Simulation :
-	def __init__ (self, terrain, nbObstacle, rayonRouesCm,rayonDuRobotCm, capteurDistance,vMaxTourParSec) :
+	def __init__ (self, rayonRouesCm,rayonDuRobotCm, capteurDistance,vMaxTourParSec) :
 		"""
 		"""
-		self.terrain = terrain
 		self.robot = Robot(rayonRouesCm, rayonDuRobotCm, capteurDistance, vMaxTourParSec)
-        cpt = 1
-        self.listObs = dict()
-        #while(cle<=nbstacle): 
-            #valX = 
-            #valY = 
-            #self.listOns[cpt] = Obstacle(10,)
+        
+
+
+
 
 
