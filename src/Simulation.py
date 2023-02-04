144c2c4bc8f2cac78400a0e7a45eb03f38d2826f
Warintara Munsup <Cartooneiei555@hotmail.com>
2023-02-04 22:41:17 +0100

    Ajout obstacles dans simulation

--- src/Simulation.py
+++ src/Simulation.py
@@ -4,11 +4,16 @@ from Roue import *
 from Capteur_de_distance import *
 
 class Simulation :
-	def __init__ (self, rayonRouesCm,rayonDuRobotCm, capteurDistance,vMaxTourParSec) :
-		"""
-		"""
-		self.robot = Robot(rayonRouesCm, rayonDuRobotCm, capteurDistance, vMaxTourParSec)
-        
+    def __init__ (self, rayonRouesCm,rayonDuRobotCm, capteurDistance,vMaxTourParSec) :
+        """
+        """
+        mur_x = 10
+        mur_y = 10
+        self.robot = Robot(rayonRouesCm, rayonDuRobotCm, capteurDistance, vMaxTourParSec)
+        obs1 = Obstacle(6,2,2)
+        obs2 = Obstacle(3,4,7,6)
+
+    
 
 
 
