166aa00bba00c0c971cb26911dd60687d6939058
massyl12 <benganamassyl56@gmail.com>
2023-02-05 19:53:50 +0100

    .

--- src/Capteur_de_distance.py
+++ src/Capteur_de_distance.py
@@ -3,3 +3,15 @@ class Capteur_de_distance :
     def __init__(self, distanceCaptable,distanceObstacle) :
        self.distanceCaptable=distanceCaptable
        self.distanceObstacle=distanceObstacle
+    
+    def distance(self,robot,obstacle):
+        """
+        Permet de calculer la distance entre le robot de l'obstable passés en parametre
+        """
+        xr = robot.pos_x
+        yr = robot.pos_y
+        xo = obstacle.x
+        yo = obstacle.y
+        return sqrt((xo-xr)**2+(yo-yr)**2)
+        
+      
