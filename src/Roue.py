877cebded13a0e28062c28ab0c583bb788429ee5
zelievdm <122898750+zelievdm@users.noreply.github.com>
2023-02-07 21:41:55 +0100

    Update Roue.py

--- src/Roue.py
+++ src/Roue.py
@@ -4,14 +4,16 @@ from math import pi
 
 class Roue :
 	def __init__ (self, taille_cm, vMaxTourParSec) :
+		"""
+		Fonction d'initialisation prenant en paramètre le rayon en cm et la vitesse maximale possible pour les roues
+		"""
 		self.taille_cm = taille_cm
 		self.vMaxTourParSec = vMaxTourParSec
 		self.vTourParSec = 0
 
 	def __str__ (self) :
 		"""
-		Equivalent methode toString(Java)
-		Permet de redéfinir la methode print(monInstance)
+		Fonction de redéfinition de la methode print(monInstance)
 		""" 
 		res = "Roue de taille " + str(self.taille_cm) + " cm"
 		if self.vTourParSec == 0: # Si la roue est à l'arret
@@ -23,8 +25,9 @@ class Roue :
 	
 	def setVitesse(self,vitesseVoulue_kmh) :
 		"""
-		Formule de conversion de la vitesse v en km/h en vitesse de rotation n en tr/s :
-		N=(5*v)/(36*pi*rayon_en_metre)
+		Formule prenant en paramètre la vitesse voulue en km/h et calculant sa converion en vitesse de rotation n en tr/s
+		puis donnant évaluant si la vitesse demandée est possible et de donner soit cette vitesse soit la vitesse maximale aux roues
+		formule de conversion utilisée : N=(5*v)/(36*pi*rayon_en_metre)
 		"""
 		print("la vitesse de la roue était de:  ",self.vTourParSec)
 		vVoulueTourParSec= (5*vitesseVoulue_kmh)/(36*pi*self.taille_cm*0.01)
