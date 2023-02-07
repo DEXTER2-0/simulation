6b01a11bc0d474902124a2ebe0e88bd332db526b
diliatighrine <tighrinedilia@outlook.fr>
2023-02-07 23:50:34 +0100

    s

--- src/testUnit.py
+++ src/testUnit.py
@@ -14,16 +14,27 @@ roue.setVitesse(20)
 robot = Robot(5,20,10,50)
 
 obstacle = Obstacle(2,4,4)
+
 print("l'état du robot : " , robot.est_entrain_de_rouler())
+
 robot.roue_gauche.setVitesse(0.1)
+
 robot.roue_droite.setVitesse(0.1)
+
 robot.avancer(4,5)
+
 robot.reculer(-4,-5)
+
 robot.arreter_urgence()
+
 robot.tourner(math.pi,3)
+
 robot.accelerer(5)
+
 robot.decelerer(4)
+
 #robot.arreter()
+
 #print("les coordonnées polaires du robot : ", robot.conversion_cartesienne_vers_polaire())
 #print("les coordonnées cartesiennes du robot : ", robot.conversion_polaire_vers_cartesienne())
 
