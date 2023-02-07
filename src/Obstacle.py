17498513e216d783b8a281dd291da374dbede4a5
diliatighrine <tighrinedilia@outlook.fr>
2023-02-07 23:57:10 +0100

    r

--- src/Obstacle.py
+++ src/Obstacle.py
@@ -11,12 +11,15 @@ class Obstacle :
 		self.y=y
 	
 	def __init__ (self, rayon,x,y) :
-	                 """ 
-			   Fonction d'initialisation prenant en paramètre le rayon et les coordonnées cartésiennes d'un obstacle circulaire 
-			   """
-	                self.rayon=rayon
-		        self.x=x
-		        self.y=y
+		
+
+	        """ 
+			   Fonction d'initialisation prenant en paramètre le rayon et les coordonnées cartésiennes d'un obstacle circulaire   
+	        """
+	        self.rayon=rayon
+	        self.x=x
+	        self.y=y
+
 	def __str__ (self) : 
 		"""
 		Fonction de redefinition de la méthode print(Instance)
