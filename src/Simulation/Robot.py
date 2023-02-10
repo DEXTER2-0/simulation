8ed933a8c1c359f4410659671f07456bc2a1c642
Warintara Munsup <Cartooneiei555@hotmail.com>
2023-02-10 15:48:45 +0100

    update robot.py

--- src/Simulation/Robot.py
+++ src/Simulation/Robot.py
@@ -6,26 +6,28 @@ import math
 import numpy as np
 
 class Robot :
-	def __init__ (self, rayonRouesCm,rayonDuRobotCm,vMaxTourParSec, r=0,angle = 0, pos_x = 0, pos_y = 0,v=0,l=8,w=0, estEnTrainDeRouler = False) :
+	def __init__ (self, rayonRouesCm,rayonDuRobotCm, capteur,vMaxTourParSec,v=0,l=8,w=0) :
 		"""
-        Le robot instancie ses deux roues de la meme taille et de meme vitesse maximal
-       	v est la vitesse moyenne du robot initialisé a zero
-       	l est la distance entre les deux roues en cm 
-       	w est la orientation du robot en fonction des deux roues
-        """
-		assert(rayonRouesCm > 0) # Ne peut pas avoir un rayon < 0
+		Fonction d'initialisation prenant en paramètre le rayon des roues en cm, le rayon du robot en cm,
+		la distance maximale captable par le capteur de distance, la vitesse maximale possible pour les roues,
+		les coordonnées polaires et les coordonnées cartésiennes du robot
+		Cette fonction instancie deux roues de la même taille et de même vitesse maximale, ainsi qu'un capteur de position
+		"""
+		assert(rayonRouesCm > 0)# Ne peut pas avoir un rayon < 0
 		assert(vMaxTourParSec > 0) # Ne peut pavoir une vitesse max < 0
 		assert(rayonDuRobotCm > 0) # Ne peut pas avoir un rayon < 0
 		self.roue_gauche = Roue(rayonRouesCm, vMaxTourParSec)
 		self.roue_droite = Roue(rayonRouesCm, vMaxTourParSec)
-		#self.r=r
-		#self.angle = angle
-		self.rayonDuRobotCm = rayonDuRobotCm 
+		self.capteurDistance = Capteur_de_distance(capteur)
+		self.rayonDuRobotCm = rayonDuRobotCm
 		#self.pos_x = pos_x
-		self.estEnTrainDeRouler = estEnTrainDeRouler # Permet de savoir si le robot est en train de rouler
+		#self.pos_y = pos_y
+        #self.robot = robot
+		#self.r=r
+        #self.angle = angle
 		self.w=w
 		self.v=v
-		self.l
+		self.l	
 
 
     def avancer(self,vitesseVoulue_kmh_er,vitesseVoulue_kmh_et) :
@@ -139,25 +141,7 @@ class Robot :
             res += " est à l'arret \n"
         return res
 =======
-	def __init__ (self, rayonRouesCm,rayonDuRobotCm, capteur,vMaxTourParSec) :
-		"""
-		Fonction d'initialisation prenant en paramètre le rayon des roues en cm, le rayon du robot en cm,
-		la distance maximale captable par le capteur de distance, la vitesse maximale possible pour les roues,
-		les coordonnées polaires et les coordonnées cartésiennes du robot
-		Cette fonction instancie deux roues de la même taille et de même vitesse maximale, ainsi qu'un capteur de position
-		"""
-		assert(rayonRouesCm > 0)# Ne peut pas avoir un rayon < 0
-		assert(vMaxTourParSec > 0) # Ne peut pavoir une vitesse max < 0
-		assert(rayonDuRobotCm > 0) # Ne peut pas avoir un rayon < 0
-		self.roue_gauche = Roue(rayonRouesCm, vMaxTourParSec)
-		self.roue_droite = Roue(rayonRouesCm, vMaxTourParSec)
-		self.capteurDistance = Capteur_de_distance(capteur)
-		self.rayonDuRobotCm = rayonDuRobotCm
-		#self.pos_x = pos_x
-		#self.pos_y = pos_y
-        #self.robot = robot
-		#self.r=r
-        #self.angle = angle
+	
 
 	def est_entrain_de_rouler(self) :
 		"""
