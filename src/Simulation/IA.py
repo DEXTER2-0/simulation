fd1af53b4fe92f6ed79d0459c6a694bbeb59727b
massyl12 <benganamassyl56@gmail.com>
2023-02-10 15:27:59 +0100

    indentation

--- src/Simulation/IA.py
+++ src/Simulation/IA.py
@@ -4,7 +4,7 @@ from Roue import *
 from Capteur_de_distance import *
 
 class IA :
-    def __init__ (self, robot,r=0,angle = 0,, pos_x = 0, pos_y = 0) :
+    def __init__ (self, robot,r=0,angle = 0, pos_x = 0, pos_y = 0) :
         """
         """
         self.r=r
@@ -12,5 +12,5 @@ class IA :
         #self.vitesse_er 
         #self.vitesse_et
         self.pos_x = pos_x
-		self.pos_y = pos_y
+        self.pos_y = pos_y
         self.robot = robot
