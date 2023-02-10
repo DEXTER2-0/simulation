5cbd55496687a44c6f9e05c75aa7902b58af725e
Yassin Lahbib <yaaston94@gmail.com>
2023-02-10 19:59:33 +0100

    modif capteur de distance car robot n'a plus acces a sa position

--- src/Simulation/Capteur_de_distance.py
+++ src/Simulation/Capteur_de_distance.py
@@ -9,13 +9,13 @@ class Capteur_de_distance :
        self.distanceCaptable=distanceCaptable
     
     
-    def distance(self,robot,obstacle):
+    def distance(self,x,y,obstacle):
         """
         Fonction prenant en paramètre le robot et l'obstacle afin de calculer leur distance 
         (racine carrée des carré des différences entre les positions selon x et selon y) et la retourner.
         """
-        xr = robot.pos_x
-        yr = robot.pos_y
+        xr = x
+        yr = y
         xo = obstacle.x
         yo = obstacle.y
         return sqrt((xo-xr)**2+(yo-yr)**2)
--- src/Simulation/Test2.py
+++ src/Simulation/Test2.py
@@ -3,6 +3,7 @@ from Roue import * # Permet d'utiliser la classe Roue se trouvant dans le meme r
 import time #pour pouvoir controler le temps de la boucle while True
 from math import *
 from IA import *
+from Obstacle import *
 
 
 # Initialisation des constantes du robot
@@ -14,6 +15,8 @@ DISTANCE_ROUE_CENTRE_CM = 100 # ----->  l
 #Initialisation du Robot
 robot = Robot(RAYON_DES_ROUES_CM, RAYON_ROBOT_CM, 10,VITESSE_MAX_TOUR_PAR_SEC)
 
+#Initialisation d'un obstacle en (5,0) de rayon 2
+obstacle = Obstacle(2,5,0)
 
 #Initilaisation de l'IA
 ia = IA(robot)
@@ -25,9 +28,13 @@ ia.pos_x=1
 ia.nouvelle_position2(1)
 
 while True :
-	ia.bouger(0,200.1)
-	ia.nouvelle_position2(1)
-	time.sleep(1)
-	print(ia)
-	print(ia.robot.roue_droite.vTourParSec)
-	print(ia.robot.roue_gauche.vTourParSec)
+	# Si le capteur detecte un obstacle a 1.5  metre
+	if robot.capteurDistance.distance(ia.pos_x,ia.pos_y,obstacle) < 1.5 :
+		ia.bouger(150,150)
+		ia.nouvelle_position2(1)
+		time.sleep(1)
+		print(ia)
+		print(ia.robot.roue_droite.vTourParSec)
+		print(ia.robot.roue_gauche.vTourParSec)
+	else :
+		
