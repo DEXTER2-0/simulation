27b7dc63280298e8d789f37a23a7ec274ad82d40
Warintara Munsup <Cartooneiei555@hotmail.com>
2023-02-12 22:35:47 +0100

    update testUnit_robot.py

--- src/Modele/testUnit_robot.py
+++ src/Modele/testUnit_robot.py
@@ -1,5 +1,5 @@
 import unittest
-import Robot
+import Robot as r
 import constantes as c
 class TestRobot(unittest.TestCase):
     
@@ -7,12 +7,12 @@ class TestRobot(unittest.TestCase):
         """
         Permet d'enregistrer tous les attributs de Robot 
         """
-        self.r1 = Robot(c.RAYON_DES_ROUES_CM,c.RAYON_ROBOT_CM,10,c.VITESSE_MAX_TOUR_PAR_SEC)
-        self.r2 = Robot(5,10,10,6)
+        self.r1 = r.Robot(c.RAYON_DES_ROUES_CM,c.RAYON_ROBOT_CM,10,c.VITESSE_MAX_TOUR_PAR_SEC)
+        self.r2 = r.Robot(5,10,10,6)
     def test_init(self):
         self.assertEqual(self.r1.roue_gauche.taille_cm,3)
         self.assertEqual(self.r1.roue_droite.taille_cm,3)
-        self.assertEqual(self.r1.roue_droite.taille_cm,self.robot1.roue_gauche.taille_cm)
+        self.assertEqual(self.r1.roue_droite.taille_cm,self.r1.roue_gauche.taille_cm)
     
     if __name__ == '__main__':
         unittest.main()
