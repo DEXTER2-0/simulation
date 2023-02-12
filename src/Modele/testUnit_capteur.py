0e223feec090b2317046e734c73a818c861b0d2c
Warintara Munsup <Cartooneiei555@hotmail.com>
2023-02-12 22:44:56 +0100

    ajout setup dans testUnit_capteur

--- src/Modele/testUnit_capteur.py
+++ src/Modele/testUnit_capteur.py
@@ -1,3 +1,13 @@
 import unittest
 import Capteur_de_distance as cap
 import constantes as c
+
+class TestCapteur(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Permet d'enregistrer tous les attributs de Capteur_de_distance
+        """
+        #self.roue1
+    if __name__ == '__main__':
+        unittest.main()
