ad0eebe1ec1b22106c43ebf1dc0ddf4abf0a58c0
Yassin Lahbib <yaaston94@gmail.com>
2023-02-12 23:42:31 +0100

    lancement

--- src/Simulation/Lancement.py
+++ src/Simulation/Lancement.py
@@ -1,8 +1,9 @@
 import Simulation as simu
-from Modele import Robot Constantes Roue Obstacle 
+from Modele import Robot Roue Obstacle 
+from Modele import Constantes as cs
 from Controleur import IA
 
 #Initialisation du Robot
-robot = Robot(RAYON_DES_ROUES_CM, RAYON_ROBOT_CM, 8,VITESSE_MAX_TOUR_PAR_SEC)
+robot = Robot(cs.RAYON_DES_ROUES_CM, cs.RAYON_ROBOT_CM, 8,cs.VITESSE_MAX_TOUR_PAR_SEC)
 
-simulation = simu.Simulation()
+simulation = simu.Simulation(robot)
