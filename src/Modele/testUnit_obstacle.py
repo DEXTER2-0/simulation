41755f3429b7ff0c737dae343acb95be99da0e71
Warintara Munsup <Cartooneiei555@hotmail.com>
2023-02-12 22:43:59 +0100

    ajout setup dans testUnitObstacle

--- src/Modele/testUnit_obstacle.py
+++ src/Modele/testUnit_obstacle.py
@@ -1,3 +1,13 @@
 import unittest
 import Obstacle as ob
 import constantes as c
+
+class TestObstacle(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Permet d'enregistrer tous les attributs de Obstacle
+        """
+        #self.roue1
+    if __name__ == '__main__':
+        unittest.main()
