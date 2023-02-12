a998095ecf3b51189c5c7a551e40948c933895f1
Warintara Munsup <Cartooneiei555@hotmail.com>
2023-02-12 22:43:02 +0100

    ajout setup dans testUnitRoue

--- src/Modele/testUnit_roue.py
+++ src/Modele/testUnit_roue.py
@@ -1,3 +1,13 @@
 import unittest
 import Roue as ro
 import constantes as c
+
+class TestRoue(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Permet d'enregistrer tous les attributs de Roues
+        """
+        #self.roue1
+    if __name__ == '__main__':
+        unittest.main()
