14f81b5534d52d015d0aa2d5bf4aee4d41b28679
Warintara Munsup <Cartooneiei555@hotmail.com>
2023-02-13 11:09:51 +0100

    update testUnit_obstacle

--- src/Modele/testUnit_obstacle.py
+++ src/Modele/testUnit_obstacle.py
@@ -8,12 +8,14 @@ class TestObstacle(unittest.TestCase):
         """
         Permet d'enregistrer tous les attributs de Obstacle
         """
-        #self.roue1
-
+        self.obs1 = ob.Obstacle(5,6,1,4) #obstacle avec le paramètre longueur*largeur
+        self.obs2 = ob.Obstacle(3,5,4) #obstacle avec le paramètre rayon
     def test_init(self):
-        """
-        """
+        self.assertEqual(self.obs1.longueur,5)
+        self.assertEqual(self.obs1.longueur,6)
+        self.assertEqual(self.obs2.rayon,3)
 
     if __name__ == '__main__':
         unittest.main()
 #pour tester -> python3 .m unittest nom_de_module.nomdefichier -v
+#constructeur avec longueur et largeur ne marche pas ?
