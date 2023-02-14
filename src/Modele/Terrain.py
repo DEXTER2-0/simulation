e27d783f75978f6b96f899aacd6c8bdd3fc8b272
Warintara Munsup <Cartooneiei555@hotmail.com>
2023-02-14 23:21:43 +0100

    update testUnit_terrain

--- src/Modele/Terrain.py
+++ src/Modele/Terrain.py
@@ -1,5 +1,5 @@
 class Terrain:
-        def __init__ (self, WIDTH_MIN, WIDTH_MAX, HEIGHT_MIN, HEIGHT_MAX, longueur_mur,liste_obstacle) :
+        def __init__ (self, WIDTH_MIN, WIDTH_MAX, HEIGHT_MIN, HEIGHT_MAX,liste_obstacle) :
             self.WIDTH_MIN = WIDTH_MIN
             self.WIDTH_MAX = WIDTH_MAX
             self.HEIGHT_MIN = HEIGHT_MIN
--- src/TestUnit/testUnit_terrain.py
+++ src/TestUnit/testUnit_terrain.py
@@ -1,16 +1,24 @@
 import unittest
-from Modele import Terrain
+from Modele import Terrain as t
+from Modele import constantes as c
 
 class TestTerrain(unittest.TestCase):
     def setUp(self):
         """
-        Permet d'enregistrer tous les attributs de IA
+        Permet d'enregistrer tous les attributs de Terrain
         """
+        self.terrain1 = t.Terrain(0,50,0,50,[])
         
 
     def test_init(self):
         """
+        vérifier si les variables de SetUp ont été bien enregistré
         """
+        self.assertEqual(self.terrain1.WIDTH_MIN,0)
+        self.assertEqual(self.terrain1.WIDTH_MAX,50)
+        self.assertEqual(self.terrain1.HEIGHT_MIN,0)
+        self.assertEqual(self.terrain1.HEIGHT_MAX,50)
+        self.assertListEqual(self.terrain1.liste_obstacle,[])
 
     if __name__ == '__main__':
         unittest.main()
