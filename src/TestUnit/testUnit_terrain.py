a310c64da3ce4bb7e9ed8879dc8b652b61b38bb2
Warintara Munsup <Cartooneiei555@hotmail.com>
2023-02-14 23:02:46 +0100

    ajout testUnit_terrain

---
+++ src/TestUnit/testUnit_terrain.py
@@ -0,0 +1,17 @@
+import unittest
+from Modele import Terrain
+
+class TestTerrain(unittest.TestCase):
+    def setUp(self):
+        """
+        Permet d'enregistrer tous les attributs de IA
+        """
+        
+
+    def test_init(self):
+        """
+        """
+
+    if __name__ == '__main__':
+        unittest.main()
+#pour tester -> python3 .m unittest nom_de_module.nomdefichier -v
