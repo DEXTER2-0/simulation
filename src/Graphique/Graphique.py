9e61dd41e755ae6d23aa206c1b08231dceb0ae1a
Yassin Lahbib <yaaston94@gmail.com>
2023-02-14 18:39:40 +0100

    Test Graphique

--- src/Graphique/Graphique.py
+++ src/Graphique/Graphique.py
@@ -11,27 +11,28 @@ from math import *
 
 ##----- Importation des Modules -----##
 from tkinter import * 
-##----- Création de la fenetre -----##
-fen = Tk() 
-canvas = Canvas(fen, width = cs.WIDTH, height = cs.HEIGHT, bg = 'yellow') #fentre graphique
-canvas.pack(fill="both", expand=True)
+
 
 class Graphique : 
-    def __init__ (self, simulation, ia, robot, terrain, duree_boucle):
+    def __init__ (self, simulation):
         self.simulation = simulation
         self.ia = ia
-        self.robot = robot
-        self.terrain = terrain
-        self.duree_boucle = duree_boucle
         # Les coordonnées (Permet de placer le robot au milieu de la fenetre)
-        ia.pos_x = terrain.WIDTH/2
-        ia.pos_y = terrain.HEIGHT/2
+        simulation.ia.pos_x = simulation.terrain.WIDTH/2
+        simulation.ia.pos_y = simulation.terrain.HEIGHT/2
 
+    def lancer_fenetre():
+        ##----- Création de la fenetre -----##
+        fen = Tk() 
+        canvas = Canvas(fen, width = cs.WIDTH, height = cs.HEIGHT, bg = 'yellow') #fentre graphique
+        canvas.pack(fill="both", expand=True)
     
-    def initialiser():
+
+
+    #def initialiser():
         # Permet de représenter le robot sur tkinter
-        representation_robot = canvas.create_oval(ia.pos_x - ia.rayonDuRobotCm , ia.pos_y - ia.rayonDuRobotCm,ia.pos_x + ia.robot.rayonDuRobotCm, ia.pos_y + ia.robot.rayonDuRobotCm, width=ia.robot.l, fill="purple")
+     #   representation_robot = canvas.create_oval(ia.pos_x - ia.rayonDuRobotCm , ia.pos_y - ia.rayonDuRobotCm,ia.pos_x + ia.robot.rayonDuRobotCm, ia.pos_y + ia.robot.rayonDuRobotCm, width=ia.robot.l, fill="purple")
 
         # orientation du robot
-        orientation = canvas.create_line(ia.pos_x,ia.pos_y, ia.pos_x+cos(ia.angle)*15, ia.pos_y+sin(ia.angle)*15, width=2,fill="black")
+      #  orientation = canvas.create_line(ia.pos_x,ia.pos_y, ia.pos_x+cos(ia.angle)*15, ia.pos_y+sin(ia.angle)*15, width=2,fill="black")
 
