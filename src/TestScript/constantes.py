53dae829042cff808cc7e81dddf31e7820982ddd
Yassin Lahbib <yaaston94@gmail.com>
2023-02-17 22:17:15 +0100

    organisation constantes

--- src/TestScript/constantes.py
+++ src/TestScript/constantes.py
@@ -1,8 +1,20 @@
+###--------------- PROPRIETE ROBOT -------------------------###
+RAYON_ROBOT_CM=5 #en cm
+
+
+###--------------- PROPRIETE ROUE ---------------------------###
 RAYON_DES_ROUES_CM=1 #en cm
 VITESSE_MAX_TOUR_PAR_SEC=30 #en km/h
-RAYON_ROBOT_CM=5 #en cm
-WIDTH = 600 # axe des x
-HEIGHT = 600 # axe des y
 V_ANGULAIRE_G = 250#250 #250.17
 V_ANGULAIRE_D = 250
+
+
+###--------------- PROPRIETE CAPTEUR_DISTANCE ----------------###
 DISTANCE_MIN_ARRET = 4 #en metre
+
+
+###--------------- PROPRIETE TERRAIN -------------------------###
+WIDTH = 600 # axe des x
+HEIGHT = 600 # axe des y
+
+
