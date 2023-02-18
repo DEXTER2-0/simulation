bf4cf2d838938d7f81d948a6bd51f170f8278a83
Yassin Lahbib <yaaston94@gmail.com>
2023-02-18 15:49:49 +0100

    TestSimulation al'aire de marcher

--- src/TestScript/constantes.py
+++ src/TestScript/constantes.py
@@ -10,7 +10,7 @@ V_ANGULAIRE_D = 250
 
 
 ###--------------- PROPRIETE CAPTEUR_DISTANCE ----------------###
-DISTANCE_MIN_ARRET = 5 #en metre (mais faudra mettre en en cm)
+DISTANCE_MIN_ARRET = 4 #en metre (mais faudra mettre en en cm)
 DISTANCE_CAPTABLE = 20+RAYON_ROBOT_CM#en metre (mais faudra mettre en en cm)
 
 ###--------------- PROPRIETE TERRAIN -------------------------###
