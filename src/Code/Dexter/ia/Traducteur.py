d631d4ebe6c5b13861f6f1687346c6563da9975c
Munsup Warintara <cartooneiei55@hotmail.com>
2023-03-02 16:27:43 +0100

    ajout un paramètre dans init de Traducteur

--- src/Code/Dexter/ia/Traducteur.py
+++ src/Code/Dexter/ia/Traducteur.py
@@ -1,5 +1,5 @@
 class Traducteur :
-    def __init__ (ia,robot_reel,robot_sim):
+    def __init__ (self,ia,robot_reel,robot_sim):
         self.ia = ia 
         self.robot_reel = robot_reel
         self.robot_sim = robot_sim    
