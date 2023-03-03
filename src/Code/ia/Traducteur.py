2a6e8b213e735ab2d5b345ebec5146748990b6ca
zelievdm <zelie.gabrielle.vandermeer@orange.fr>
2023-03-03 16:48:26 +0100

    classe Taducteur_Simualtion

--- src/Code/ia/Traducteur.py
+++ src/Code/ia/Traducteur.py
@@ -2,4 +2,8 @@ class Traducteur :
     def __init__ (self,ia,robot_reel,robot_sim):
         self.ia = ia 
         self.robot_reel = robot_reel
-        self.robot_sim = robot_sim    
+        self.robot_sim = robot_sim
+
+class Traducteur_Simulation:
+    def __init__(self,robot):
+        self.robot=robot
