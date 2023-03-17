969d31d12baa77618d991cae0b928c7b3db413ae
Munsup Warintara <cartooneiei55@hotmail.com>
2023-03-17 15:19:43 +0100

    ajout Thread dans simulation.py

--- src/Code/simulation/Simulation.py
+++ src/Code/simulation/Simulation.py
@@ -5,88 +5,90 @@ import time #pour pouvoir controler le temps de la boucle while True
 import numpy as np
 from math import *
 import logging
+import threading
 #logging.basicConfig(filename='Simulation.log', filemode='w', level=logging.DEBUG)
 
 
 
-class Simulation : 
-	def __init__ (self, robot,terrain,duree_boucle,pos_x=0,pos_y=0,r=0,angle=0) :
-		"""     
+class Simulation(Thread) : 
+    def __init__ (self, robot,terrain,duree_boucle,pos_x=0,pos_y=0,r=0,angle=0) :
+        """     
 	:param robot : Robot utilisé
 	:param terrain : Terrain utilisé
 	:param duree_boucle : duree de simulation
-	"""
-		#self.mur_x = range(10) 
-		#self.mur_y = range(10)
-		self.robot = robot
-		self.terrain = terrain
-		self.duree_boucle = duree_boucle
-		self.pos_x=pos_x
-		self.pos_y=pos_y
-		self.r=r
-		self.angle = angle
+    """
+        #self.mur_x = range(10) 
+        #self.mur_y = range(10)
+        self.robot = robot
+        self.terrain = terrain
+        self.duree_boucle = duree_boucle
+        self.pos_x=pos_x
+        self.pos_y=pos_y
+        self.r=r
+        self.angle = angle
 	
 
-	#partie capteur de distance :
+	 #partie capteur de distance :
 	self.capteurOn = False
-	#coordonnées du dernier point capté par le capteur de distance 
+	  #coordonnées du dernier point capté par le capteur de distance 
 
 	self.lastX = 0  
 	self.lastY=0
 
 
+    
 	
+        #self.obs1 = Obstacle(6,2,2)
+        #self.obs2 = Obstacle(3,4,7)
+    
+
+    def run (self):
+	    self.running = True
+	    while self.running :
+		    self._lastTime = time.time()
+		    time.sleep(self._wait)
+		    self._dT = time.time() - self._lastTime
+		    self.actualiser ()
+    def capterDistance(self,robot):
+	    Distance=0
+	    Vect0=(cos(angle))
+	    Vect1=sin(-angle)
+	    RayonCoord=(pos_x + Vect0 * robot.rayonDuRobotCm,pos_y + Vect1 * robot.rayonDuRobotCm)
+
+	    for i in range(0,len(self.terrain.getListeObstacles())):
+		    if self.terrain.getObstacle(i).Capte(RayonCoord[0] , RayonCoord[1]):
+			    self.SensorOn= True
+			    self.newPos_x = RayonCoord[0]	
+
+
+   def run(self):
+	   self.run=True
+
+   def collision(self):
+        """
+        Suppose objet est cercle
+        """
+
+        if (self.pos_x<=0) or (self.pos_y<=0) or (self.pos_x>=cs.WIDTH) or (self.pos_y>=cs.HEIGHT):
+            self.robot.setMotorDps(0, 0)
+            logging.debug("Collision détectée : arret d'urgence")
+            return 1
+        for obstacle in self.terrain.liste_obstacle: #pour chaque obstacle
+            d=np.sqrt((self.pos_x-obstacle.x)**2+(self.pos_y-obstacle.y)**2) #distance euclidienne entre le robot et l'obstacle
+            if(d<=(self.robot.rayonDuRobotCm+obstacle.longueur)): # collision de deux cercles
+                self.robotsetMotorDps(0, 0)
+                logging.debug("Collision détectée : arret d'urgence")
+                return 1
+            #elif (d<=(self.ia.robot.rayon)): # collision d'un cercle et d'un rectangle A COMPLETER
+             #   self.ia.robot.arret_urgence()
 	
-		#self.obs1 = Obstacle(6,2,2)
-		#self.obs2 = Obstacle(3,4,7)
-		# 
-	def run (self):
-		self.running = True
-		while self.running :
-			self._lastTime = time.time()
-			time.sleep(self._wait)
-			self._dT = time.time() - self._lastTime
-			self.actualiser ()
-	def capterDistance(self,robot):
-		Distance=0
-		Vect0=(cos(angle))
-		Vect1=sin(-angle)
-		RayonCoord=(pos_x + Vect0 * robot.rayonDuRobotCm,pos_y + Vect1 * robot.rayonDuRobotCm)
-
-		for i in range(0,len(self.terrain.getListeObstacles())):
-			if self.terrain.getObstacle(i).Capte(RayonCoord[0] , RayonCoord[1]):
-				self.SensorOn= True
-				self.newPos_x = RayonCoord[0]	
-
-
-#	def run(self):
-#	   self.run=True
-	   
-	def collision(self):
-		"""
-		Suppose objet est cercle
-		"""
-
-		if (self.pos_x<=0) or (self.pos_y<=0) or (self.pos_x>=cs.WIDTH) or (self.pos_y>=cs.HEIGHT):
-			self.robot.setMotorDps(0, 0)
-			logging.debug("Collision détectée : arret d'urgence")
-			return 1
-		for obstacle in self.terrain.liste_obstacle: #pour chaque obstacle
-			d=np.sqrt((self.pos_x-obstacle.x)**2+(self.pos_y-obstacle.y)**2) #distance euclidienne entre le robot et l'obstacle
-			if(d<=(self.robot.rayonDuRobotCm+obstacle.longueur)): # collision de deux cercles
-				self.robotsetMotorDps(0, 0)
-				logging.debug("Collision détectée : arret d'urgence")
-				return 1
-			#elif (d<=(self.ia.robot.rayon)): # collision d'un cercle et d'un rectangle A COMPLETER
-			 #   self.ia.robot.arret_urgence()
-	
-	def nouvelle_position2(self,duree):
-		"""
+    def nouvelle_position2(self,duree):
+        """
 	:param duree : duree passee depuis le dernier calcul de la position
-		Doit etre appelé apres la methode bouger() pour pouvoir mettre a jours les 
-		oordonées du robot ainsi que son angle d'orientation					                      
-		"""
+        Doit etre appelé apres la methode bouger() pour pouvoir mettre a jours les 
+        oordonées du robot ainsi que son angle d'orientation					                      
+        """
 
-		self.pos_x = self.pos_x + self.IAEvite.v * cos(self.angle)*duree
-		self.pos_y = self.pos_y + self.IAEvite.v * sin(self.angle)*duree
-		self.angle = self.angle + self.IAEvite.new_orientation * duree
+        self.pos_x = self.pos_x + self.IAEvite.v * cos(self.angle)*duree
+        self.pos_y = self.pos_y + self.IAEvite.v * sin(self.angle)*duree
+        self.angle = self.angle + self.IAEvite.new_orientation * duree
