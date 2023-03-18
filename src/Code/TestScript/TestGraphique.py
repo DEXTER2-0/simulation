f389a31b15d008e9ee1cb7e059f46379529e229b
Munsup Warintara <cartooneiei55@hotmail.com>
2023-03-18 16:26:57 +0100

    supprime capteur de robot et MAJ unittest

--- src/Code/TestScript/TestGraphique.py
+++ src/Code/TestScript/TestGraphique.py
@@ -1,5 +1,4 @@
-
-import time #pour pouvoir controler le temps de la boucle while True
+"""import time #pour pouvoir controler le temps de la boucle while True
 
 from Code.simulation import Simulation as simu
 from Code.simulation import Robot as rb 
@@ -16,10 +15,10 @@ import time #pour pouvoir controler le temps de la boucle while True
 
 
 #Initialisation du Robot
-robot = rb.Robot(cs.RAYON_DES_ROUES_CM, cs.RAYON_ROBOT_CM,cs.DISTANCE_CAPTABLE ,cs.VITESSE_MAX_RAD_PAR_SEC)
+robot = rb.Robot(cs.RAYON_DES_ROUES_CM, cs.RAYON_ROBOT_CM,cs.DISTANCE_CAPTABLE ,cs.VITESSE_MAX_DEG_PAR_SEC)
 
 #Initilaisation de l'IA
-ia = ia.IA_eviter(robot)
+#ia = ia.IA_eviter(robot)
 
 #Initialisation d'une liste d'obstacle
 obstacle1 = obs.Obstacle(8,400,300)
@@ -36,7 +35,7 @@ liste_obstacle.append(obstacle1)
 terrain = ter.Terrain(0,cs.WIDTH,0,cs.HEIGHT, liste_obstacle)
 
 #initialisation de la Code.simulation
-Code.simulation = simu.Code.simulation(ia,robot,terrain,0.1)
+simulation = simu.Simulation(robot,terrain,0.1)
 
 fen = tk.Tk() 
 
@@ -45,7 +44,7 @@ canvas_fenetre = tk.Canvas(fen, width = cs.WIDTH, height = cs.HEIGHT, bg = 'yell
 canvas_fenetre.pack(fill="both", expand=True)
 
 #Initialisation d'un graphique récuperant les données de la Code.simulation et les recopie sur une fentre
-graph = gr.Graphique(canvas_fenetre,Code.simulation)
+graph = gr.Graphique(canvas_fenetre,simulation)
 
 #placer le robot au milieu de la fenetre
 #graph.placer_robot_milieu(Code.simulation)
@@ -53,7 +52,7 @@ graph = gr.Graphique(canvas_fenetre,Code.simulation)
 #Lancement de la Code.simulation avec un update de la partie graphique qui se met a jours quand la Code.simulation change
 while True :
     #MAJ de la simu
-    Code.simulation.update_Code.simulation()
+    simulation.update_Code.simulation()
     #MAJ des coordonnes recu par la Code.simulation
     graph.update()
     #MAJ de l'affichage graphique
@@ -65,3 +64,4 @@ while True :
 
 
 #graph.afficher()
+"""
--- src/Code/TestScript/testUnit_IA.py
+++ src/Code/TestScript/testUnit_IA.py
@@ -5,14 +5,32 @@ from Code.simulation import Terrain as tr
 from Code.ia import IA as ia
 from Code.simulation import Obstacle as ob
 from Code.simulation import Simulation as sm
+
+
+
+
+class Test_IA(unittest.TestCase):
+    def setUp(self,list_ia):
+        self.robot = rb.Robot(cs.RAYON_DES_ROUES_CM,cs.RAYON_ROBOT_CM,cs.VITESSE_MAX_DEG_PAR_SEC)
+        self.ia_avance = ia.IA_avancer(self.robot)
+        self.ia_tourne = ia.IA_tourner(self.robot)
+        self.ia_evite = ia.IA_eviter(self.robot,self.ia_avance,self.ia_tourne)
+        self.list = [self.ia_avance,self.ia_tourne,self.ia_evite]
+        self.ia = ia.IA(self.list)
+
+    def test_init(self):
+        self.assertIn(self.ia_evite,self.list)
+        self.assertIn(self.ia_tourne,self.list)
+        self.assertIn(self.ia_avance,self.list)
+
 class TestIA_avancer(unittest.TestCase):
 
     def setUp(self):
         """
         Permet d'enregistrer tous les attributs de Obstacle
         """
-        self.capteur1 = rb.Capteur_de_distance(cs.DISTANCE_CAPTABLE)
-        self.robot1 = rb.Robot(cs.RAYON_DES_ROUES_CM,cs.RAYON_ROBOT_CM,self.capteur1,cs.VITESSE_MAX_DEG_PAR_SEC)
+        #self.capteur1 = rb.Capteur_de_distance(cs.DISTANCE_CAPTABLE)
+        self.robot1 = rb.Robot(cs.RAYON_DES_ROUES_CM,cs.RAYON_ROBOT_CM,cs.VITESSE_MAX_DEG_PAR_SEC)
         self.ia = ia.IA_avancer(self.robot1)
         self.obs1 = ob.Obstacle(2,22,0)
         self.obs2 = ob.Obstacle(4,15,5)
@@ -27,10 +45,10 @@ class TestIA_avancer(unittest.TestCase):
         self.assertEqual(self.robot1.roue_gauche.taille_cm,cs.RAYON_DES_ROUES_CM) # pour le robot
         self.assertEqual(self.robot1.roue_gauche.taille_cm,self.robot1.roue_droite.taille_cm)
         self.assertEqual(self.robot1.rayonDuRobotCm, cs.RAYON_ROBOT_CM)
-        self.assertEqual(self.ia.robot.w, 0) 
-        self.assertEqual(self.robot1.w,0)
+        self.assertEqual(self.ia.robot.new_orientation, 0) 
+        self.assertEqual(self.robot1.new_orientation,0)
         self.assertEqual(self.ia.robot.v,self.robot1.v)
-        self.assertEqual(self.ia.robot.w,self.robot1.w)
+        self.assertEqual(self.ia.robot.new_orientation,self.robot1.new_orientation)
         self.assertIn(self.obs1,self.listObs)
         self.assertIn(self.obs2,self.listObs)
         self.assertIn(self.obs3,self.listObs)
