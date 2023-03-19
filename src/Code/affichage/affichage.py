91533223bad707516de326baf50c12f954f46b67
diliatighrine <tighrinedilia@outlook.fr>
2023-03-19 23:07:54 +0100

    update affichage

--- src/Code/affichage/affichage.py
+++ src/Code/affichage/affichage.py
@@ -3,6 +3,7 @@ import time
 from math import *
 import pygame
 from .. import simulation 
+import os
 
 #colors 
 BLACK = (0, 0, 0, 255)
@@ -28,6 +29,66 @@ class Affichage(Thread):
 
 
 	
+
+	def boucle(self,fps):
+		""" 	Cette boucle permet d'actualiser l'affichage a chaque pas de temps   """
+
+		self.run = True
+
+		while self.run : 
+			self.update()
+			time.sleep(1./fps)
+	
+
+
+	def events(self) :
+		""" sert a recuperer les evenements """
+
+		for event in pygame.event.get() : 
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				exit(0)
+			elif event.type == pygame.KEYDOWN :
+				self.pressed[event.key] = True
+			elif event.type == pygame.KEYUP : 
+				self.pressed[event.key] = False 
+
+
+
+	def update(self,fps):
+
+		""" Permet de réafficher et de redessiner le robot et les obstacles """
+
+		#dessiner la balise : 
+
+		self._screen.blit(self._trace, (0, 0))
+		#dessiner les obstacles : 
+
+		for obs in self.simulation.terrain.getListeObstacles():
+			pygame.draw.circle(self._trace, RED, (obs.x + self._screen.get_size()[0]/2, obs.y + self._screen.get_size()[0]/2), obs.rayon)
+
+
+
+
+
+	       #on dessine le robot : 
+
+	       os.chdir(os.path.dirname(os.path.abspath(__file__)))
+	       im1 = pygame.image.load("robot.png").convert_alpha()
+	       im1 = pygame.transform.scale(image_pas_tournee, (image_pas_tournee.get_width()/20 * robot.rayonDuRobotCm, im1.get_height()/20 * robot.rayonDuRobotCm))
+
+	       im2 = pygame.transform.rotate(im1, degrees(robot.angle))
+
+
+	       #affichage du robot : 
+	       pygame.draw.circle(self._trace, GREEN, (robot.x + self._trace.get_size()[0]/2, robot.y + self._trace.get_size()[0]/2), 2)
+
+	       #mettre a jour l'ecran 
+
+	       pygame.display.update()
+
+	       #femeture de la fenetre :
+	       self.events()
 
 
 
