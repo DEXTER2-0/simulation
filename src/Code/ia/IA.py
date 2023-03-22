b9142175cb60c35bcdfb00653efda1bf3cdae31d
zelievdm <122898750+zelievdm@users.noreply.github.com>
2023-03-22 11:48:54 +0100

    MAj des unités IA_avancer

--- src/Code/ia/IA.py
+++ src/Code/ia/IA.py
@@ -60,8 +60,8 @@ class IA_avancer :
 		self.robot.setMotorDps(cs.V_ANGULAIRE_G,cs.V_ANGULAIRE_D)
 		self.fonctionne=True
 		self.arret=False
-		self.robot.v=(cs.RAYON_DES_ROUES_CM/2)*(cs.V_ANGULAIRE_G+cs.V_ANGULAIRE_D)
-		self.robot.new_orientation=(cs.RAYON_DES_ROUES_CM/cs.RAYON_ROBOT_CM)*(cs.V_ANGULAIRE_G-cs.V_ANGULAIRE_D)
+		self.robot.v=(cs.RAYON_DES_ROUES_CM*0.01/2)*(cs.V_ANGULAIRE_G*(360/(2*pi))+cs.V_ANGULAIRE_D*(360/(2*pi)))
+		self.robot.new_orientation=(cs.RAYON_DES_ROUES_CM/cs.RAYON_ROBOT_CM)*((cs.V_ANGULAIRE_G*(360/(2/pi))-cs.V_ANGULAIRE_D*(360/(2*pi)))
 
 	def step(self):
 		if self.arret:
