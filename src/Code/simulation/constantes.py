18c970ee7881d6c64510b9ba48696a86abf3dd63
zelievdm <zelie.gabrielle.vandermeer@orange.fr>
2023-03-26 19:04:32 +0200

    rangement constantes.py

--- src/Code/simulation/constantes.py
+++ src/Code/simulation/constantes.py
@@ -1,15 +1,12 @@
 ###--------------- PROPRIETE ROBOT -------------------------###
 RAYON_ROBOT_CM=10 #en cm
 
-
 ###--------------- PROPRIETE ROUE ---------------------------###
 RAYON_DES_ROUES_CM=3 #en cm
 
-VITESSE_MAX_DEG_PAR_SEC= 1
+#VITESSE_MAX_DEG_PAR_SEC= 1
 V_ANGULAIRE_G = 12 #°/s
 V_ANGULAIRE_D = 12 #°/s
-
-
 
 ###--------------- PROPRIETE CAPTEUR_DISTANCE ----------------###
 DISTANCE_MIN_ARRET = 5 #en metre (mais faudra mettre en en cm)
@@ -18,5 +15,3 @@ DISTANCE_CAPTABLE = 10+RAYON_ROBOT_CM #en metre (mais faudra mettre en en cm)
 ###--------------- PROPRIETE TERRAIN -------------------------###
 WIDTH = 300 # axe des x
 HEIGHT = 300 # axe des y
-
-
