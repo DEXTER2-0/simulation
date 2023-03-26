233f3afbb5e0b37c41e0529282877ce83b597f74
zelievdm <zelie.gabrielle.vandermeer@orange.fr>
2023-03-26 19:05:38 +0200

    rangement : import inutile  Obstacle.py

--- src/Code/simulation/Obstacle.py
+++ src/Code/simulation/Obstacle.py
@@ -1,4 +1,3 @@
-#coding: utf-8
 import logging
 class Obstacle :
 
@@ -8,14 +7,12 @@ class Obstacle :
 			: 2 arguments : création un obstacle de type mur 
 			: 3 arguments : création un obstacle de type cercle
 			: 4 arguments : création un obstacle de type rectangle
-
 		"""
 		if (len(args)<3 |len(args)>4 ):
 			if(len(args)<3):
 				logging.debug("error : il faut au moins 3 arguments")
 			else:
 				logging.debug("error : il faut au plus 4 arguments")
-		
 		elif len(args)==3 :
 			self.longueur = args[0]
 			self.largeur = args[0]
