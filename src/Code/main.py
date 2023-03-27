b2b6404df52a7569ba5e084218a95f107fcf7b76
zelievdm <zelie.gabrielle.vandermeer@orange.fr>
2023-03-27 23:24:09 +0200

    resolution probleme main.py

--- src/Code/main.py
+++ src/Code/main.py
@@ -6,41 +6,28 @@ from Code.ia  import IA as ia
 from Code.affichage import affichage as af
 from Code.simulation  import Terrain as ter
 from Code.simulation import Robot as rb
-
+from math import radians
 from time import time as time
-#cree les obstacles
-#obstacle4 = obs.Obstacle(10,20,40)
-liste_obstacle = []
 
-#initialise le robot
-Dexter=rb.Robot(cs.RAYON_DES_ROUES_CM,5,cs.VITESSE_MAX_DEG_PAR_SEC,cs.DISTANCE_CAPTABLE)
-
-#initialise le terrain
-Terrain=ter.Terrain(-300,cs.WIDTH,-300,cs.HEIGHT, liste_obstacle)
-
-#commandes pour que le robot tourne
-
-
-
-
-
-
-Simu=simu.Simulation(Dexter,Terrain,0.01)
-#initialisation le traducteur 
-trad = tr.Traducteur_Simulation(Simu,Dexter)
-
-IA_tourne = ia.IA_tourner(trad,90)
-
-#commandes pour que le robot avance
-IA_avance = ia.IA_avancer(trad,50000)
-
-Affichage=af.Affichage(Simu,Terrain,Dexter,200)
-
-IA = ia.IA(trad,[IA_tourne],0.01)
-#IA = ia.IA(trad,[IA_avance,IA_tourne,IA_avance,IA_tourne,IA_avance,IA_tourne,IA_avance],0.01)
-
-#IA = ia.IA(Dexter,[IA_evite],0.01)
-
-Simu.start()
-Affichage.start()
-IA.start()
+if __name__=='__main__':
+    #cree les obstacles
+    #obstacle4 = obs.Obstacle(10,20,40)
+    liste_obstacle = [obs.Obstacle(10,50,0)]
+    #initialise le robot
+    Dexter=rb.Robot(cs.RAYON_DES_ROUES_CM,5,cs.VITESSE_MAX_DEG_PAR_SEC,cs.DISTANCE_CAPTABLE)
+    #initialise le terrain
+    Terrain=ter.Terrain(-300,cs.WIDTH,-300,cs.HEIGHT, liste_obstacle)
+    #commandes pour que le robot tourne
+    Simu=simu.Simulation(Dexter,Terrain,0.01)
+    #initialisation le traducteur 
+    trad = tr.Traducteur_Simulation(Simu,Dexter)
+    IA_tourne = ia.IA_tourner(trad,90)
+    #commandes pour que le robot avance
+    IA_avance = ia.IA_avancer(trad,10_000)
+    Affichage=af.Affichage(Simu,Terrain,Dexter,200)
+    #IA = ia.IA(trad,[IA_tourne],0.01)
+    IA = ia.IA(trad,[IA_avance,IA_tourne,IA_avance,IA_tourne,IA_avance,IA_tourne,IA_avance],0.01)
+    #IA = ia.IA(Dexter,[IA_evite],0.01)
+    Affichage.start()
+    Simu.start()
+    IA.start()
