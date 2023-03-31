7b1049221f4d6a2cccc2e49eedd4670740df218c
zelievdm <zelie.gabrielle.vandermeer@orange.fr>
2023-03-31 15:39:44 +0200

    classe Vecteur Vecteur.py

--- src/Code/simulation/Vecteur.py
+++ src/Code/simulation/Vecteur.py
@@ -5,3 +5,20 @@ class Point:
         self.x = x
         self.y = y
 
+class Vecteur:
+    def __init__(self, p1, p2):
+        """
+        :param p1: point 1
+        :param p2: point 2
+        """
+        self.vect = (p2.x - p1.x, p2.y - p1.y)
+    
+    def get_vect_from_angle(self,angle):
+        """
+        :param angle: angle
+        Vecteur direction depuis un angle
+        """
+        angle = radians(angle)
+        return Vecteur(Point(0, 0), Point(round(cos(angle), 2), round(sin(angle), 2)))
+
+    
