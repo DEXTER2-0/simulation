ff657bf4777380bda3f493a79917802b756eb589
Yassin Lahbib <yaaston94@gmail.com>
2023-03-31 18:21:17 +0200

    test unitaire capteur de distance + modification du capteur de distance

--- src/Code/TestScript/testUnit_capteur.py
+++ src/Code/TestScript/testUnit_capteur.py
@@ -1,9 +1,11 @@
 import unittest
-from Code.Capteur_de_distance import Capteur_de_distance as cap
-from ..Dexter.Robot import Robot as rob
-from ..Dexter.Obstacle import Obstacle as ob
-from ..Dexter.constantes import constantes as c
-from ..Controleur.IA import IA as i
+from Code.simulation import Robot as r
+from Code.simulation import Capteur_de_distance
+#from Code.Capteur_de_distance import Capteur_de_distance as cap
+from Code.simulation import Obstacle as ob
+from Code.simulation import Vecteur as vec
+from Code.simulation import constantes as cs
+
 
 class TestCapteur(unittest.TestCase):
 
@@ -11,24 +13,30 @@ class TestCapteur(unittest.TestCase):
         """
         Permet d'enregistrer tous les attributs de Capteur_de_distance
         """
-        self.capteur = cap.Capteur_de_distance(10)
-        self.robot = rob.Robot(3,7,self.capteur,10)
-        self.ia = i.IA(self.robot)
-        self.obs1 = ob.Obstacle(5,6,1,4) #création d'un obstacle du type rectangle
-        self.obs2 = ob.Obstacle(3,5,4) #création d'un obstacle du type cercle
-        self.obs3 = ob.Obstacle(50,50) #création d'un obstacle du type mur
-        listObs = [self.obs1,self.obs2,self.obs3]
-    def test_init(self):
-        self.assertEqual(self.capteur.distanceCaptable,10)
+        self.capteur = Capteur_de_distance(10)
+        self.robot = r.Robot(3,5,200,10,vec.Point(0,0),0)
+        #self.ia = i.IA(self.robot)
+        self.obs1 = ob.Obstacle(1,[7,7],1)
+        self.obs2 = ob.Obstacle(1,[0,15],2) 
+        self.obs3 = ob.Obstacle(1,[0,250],1) 
+        self.listObs = [self.obs1,self.obs2,self.obs3]
+    #def test_init(self):
+     #   print("test_INIT")
+      #  self.assertEqual(self.capteur.distanceCaptable,10)
 
-    def test_distance(self):
-        self.capteur.distance(self.ia.pos_x,self.ia.pos_y,self.obs1)
-        self.capteur.distance(self.ia.pos_x,self.ia.pos_y,self.obs2)
-        self.capteur.distance(self.ia.pos_x,self.ia.pos_y,self.obs3)
+    #def test_distance(self):
+     #   print("testDistance")
+        #self.capteur.distance(self.ia.pos_x,self.ia.pos_y,self.obs1)
+        #self.capteur.distance(self.ia.pos_x,self.ia.pos_y,self.obs2)
+        #self.capteur.distance(self.ia.pos_x,self.ia.pos_y,self.obs3)
 
     def test_senseurDistance(self):
+        self.assertGreaterEqual(self.capteur.senseur_de_distance(self.robot.centre.x,self.robot.centre.y,3.14/2,0.01, self.listObs),0)
         
     if __name__ == '__main__':
+        print("MMAAINN")
+
+
         unittest.main()
 #pour tester -> python3 .m unittest nom_de_Dexter .nomdefichier -v
     
