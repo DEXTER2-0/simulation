9ceb3da83fc38bc48d2f7defd4b9ca305716ab8d
Munsup Warintara <cartooneiei55@hotmail.com>
2023-03-31 17:53:29 +0200

    tester obstacle.py

--- src/Code/TestScript/testUnit_obstacle.py
+++ src/Code/TestScript/testUnit_obstacle.py
@@ -7,7 +7,7 @@ class TestObstacle(unittest.TestCase):
         """
         Permet d'enregistrer tous les attributs de Obstacle
         """
-        self.obs1 = Obstacle(2, (5, 3),[5, 6]) #création d'un obstacle du type rectangle
+        self.obs1 = Obstacle(2, (5, 3),(5, 6)) #création d'un obstacle du type rectangle
         self.obs2 = Obstacle(1,(1,4),5) #création d'un obstacle du type cercle
         
     def test_init(self):
@@ -18,8 +18,9 @@ class TestObstacle(unittest.TestCase):
         self.assertEqual(self.obs1.type,2)
         self.assertEqual(self.obs1.longueur,5)
         self.assertEqual(self.obs1.largeur,6)
-        self.assertNotEqual(self.obs1.largeur,self.obs1.longueur)       
-
+        self.assertNotEqual(self.obs1.largeur,self.obs1.longueur)   
+        self.assertEqual(self.obs1.pos[0], 5)
+        self.assertEqual(self.obs1.pos[0], 5)
         #self.assertEqual(self.obs2.longueur,3)
         #self.assertEqual(self.obs2.largeur,3)
 
