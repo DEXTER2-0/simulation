4a557f74e087f82fd567232a71b5aadf71b7449b
Munsup Warintara <cartooneiei55@hotmail.com>
2023-04-12 09:55:26 +0200

    nettoyage Traducteur.py

--- src/Code/ia/Traducteur.py
+++ src/Code/ia/Traducteur.py
@@ -3,17 +3,6 @@ from Code.simulation.Robot import Capteur_de_distance
 from math import pi
 import time as time
 
-class Traducteur :
-    def __init__ (self,simulation,robot_reel,robot_sim):
-        """
-        :param simulation : simulation utilisee
-        :param robot_reel : robot reel utilise
-        :param robot_sim : robot simule utilise
-        """
-        self.simulation=simulation
-        self.robot_reel = robot_reel
-        self.robot_sim = robot_sim
-
 class Traducteur_Simulation:
     def __init__(self,robot,simulation):
         """
@@ -43,18 +32,6 @@ class Traducteur_Simulation:
         k, r = divmod(diff, 360)
         return k * self.robot.rayonRouesCm + (r * self.robot.rayonRouesCm) / 360
 
-    def resetdistance(self):
-        self.distance=0
-
-    def getangle(self):
-        t=time.time()
-        dt=t-self.t0
-        self.t0=t
-        self.angle+=dt*self.robot.gspeed
-    
-    def resetangle(self):
-        self.angle=0
-
     def capteur(self):
         t=time.time()
         dt=t-self.t0
@@ -75,51 +52,3 @@ class Traducteur_Simulation:
     def stop(self):
        self.robot.setMotorDps(self.robot.MOTOR_GAUCHE+self.robot.MOTOR_DROIT,0)
     
-    def reset(self,roue):
-        self.distance=self.robot.get_pos_roues()[roue]
-        self.angle=0
-        self.robot.setMotorDps(0,0)
-    
-    def evaluer_condition(self,condition):
-        """
-        :condition: condition(chaine caractere) a evaluer 
-        verifier la condition, return True si c'est verifie, Flase sinon. 
-        """
-        return (eval(condition))
-
-class Traducteur_Realite:
-    def __init__(self,robot):
-        """
-        :param robot : robot utilise
-        """
-        self.robot=robot
-    
-    def setMotorDps(self,v_gauche,v_droit):
-        """
-        :param v_gauche : vitesse de la roue gauche en deg/s
-        :param v_droite : vitesse de la roue droite en deg/s
-        """
-        self.robot.set_motor_dps(port,v_gauche)
-    
-    def getdistance(self,dt):
-        """
-        :param dt : temps ecoule depuis le dernier calcul
-        """
-        return dt*self.robot.get_motor_position()*360#*taille de la roue en m
-    
-    def resetdistance(self):
-        self.robot.offset_motor_encoder(port, offset)
-
-    def getangle(self,dt):
-        """
-        :param dt : temps ecoule depuis le dernier calcul
-        """
-        return dt*self.robot.get_motor_position()
-    
-    def resetangle(self):
-        self.robot.offset_motor_encoder(port, offset)
-    
-    #def capteur(dt):
-    #    """
-    #    :param dt : temps ecoule depuis le dernier calcul
-    #    """
