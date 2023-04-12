09343a365873e66f7ccec6915db4f69ea8e4a147
Munsup Warintara <cartooneiei55@hotmail.com>
2023-04-12 09:57:43 +0200

    nettoyage Traducteur_proxy.py

--- src/Code/ia/Traducteur_proxy.py
+++ src/Code/ia/Traducteur_proxy.py
@@ -9,7 +9,6 @@ class Traducteur(object):
 
     def __init__(self, robot):
         self.robot = robot
-
         self.ref_list = {}
 
     def debut(self, ref, port):
@@ -19,35 +18,17 @@ class Traducteur(object):
         """
         self.ref_list[ref] = self.robot.get_motor_position()[port]
 
-    def stopSim(self):
-        self.simulation.stop()
-
     def getdistance(self, ref, port):
         """
         :param int port:
         Distance parcourue pour la strat
         """
-        diff = self.robot.get_pos_roues()[port] - self.ref_list[ref]
-
+        diff = self.robot.get_motor_position()[port] - self.ref_list[ref]
         self.ref_list[ref] = self.robot.get_motor_position()[port]
-
         # Distance parcourue
         k, r = divmod(diff, 360)
-
         return k * self.robot.rayonRouesCm + (r * self.robot.rayonRouesCm) / 360
-    
-    def resetdistance(self):
-        self.distance=0
 
-    def getangle(self):
-        t=time.time()
-        dt=t-self.t0
-        self.t0=t
-        self.angle+=dt*self.robot.gspeed
-    
-    def resetangle(self):
-        self.angle=0
-    
     #gestion capteur
 
     def avance(self, speed):
@@ -73,8 +54,3 @@ class Traducteur(object):
         else:
             self.robot.set_motor_dps(self.robot.MOTOR_RIGHT, 0)
             self.robot.set_motor_dps(self.robot.MOTOR_LEFT, speed)
-
-    def reset(self,roue):
-        self.distance=self.robot.get_motor_position()[roue]
-        self.angle=0
-        self.robot.set_motor_dps(0,0)
