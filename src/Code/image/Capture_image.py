a9050f997af8ba6b1d9074e6a4f1e0d5c97221a9
Munsup Warintara <cartooneiei55@hotmail.com>
2023-04-12 11:21:50 +0200

    classe Traitement_image et classe Decision

--- src/Code/image/Capture_image.py
+++ src/Code/image/Capture_image.py
@@ -1,3 +1,6 @@
+from PIL import Image
+import cv2
+
 class Capture_image:
     def __init__(self,robot):
         self.robot=robot
@@ -9,3 +12,14 @@ class Capture_image:
     
     def stop(self):
         self.encours=False
+
+class Traitement_image: #traitement d'image afin de savoir si la balise entiere est dedans
+    def __init__(self):
+        self.image=None
+        self.balise=None
+    
+class Decision:
+    def __init__(self,robot):
+        self.robot=robot
+        self.cap=Capture_image(self.robot)
+        self.trait=Traitement_image()
