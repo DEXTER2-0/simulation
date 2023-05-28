570363d87570946839dfb8316ace6dc9db9220ab
Munsup Warintara <cartooneiei55@hotmail.com>
2023-05-28 20:44:22 +0200

    modif les imports dans testcamera.py

--- src/testcamera.py
+++ src/testcamera.py
@@ -6,9 +6,9 @@ from Code.ia  import IA as ia
 from Code.affichage import affichage as af
 from Code.simulation  import Terrain as ter
 from Code.ia.robot2IN013 import Robot2IN013 
-from Code.image import Capt 
-from Code.image import Traitement_image as ti
-from Code.image import Decision as di
+from Code.image import Capt
+from Code.image.Capture_image import Traitement_image 
+from Code.image import Decision 
 from math import radians
 from time import time as time
 import logging
@@ -30,8 +30,8 @@ if __name__=='__main__':
     #trad = tr.Traducteur_Simulation(Dexter,Simu)
     trad=proxy.Traducteur(Dexter,Simu,False)
     cam = Capt(Dexter)
-    tim = ti.Traitement_image()
-    des = di.Decision(Dexter,trad)
+    tim = Traitement_image()
+    des = Decision(Dexter,trad)
 
 
     test = []
